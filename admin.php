09235807255f99dd04a828844ab9c8895c9e2b57
satyalexis <alexismurugan@gmail.com>
2025-03-21 14:55:28 +0100

    corrections

--- admin.php
+++ admin.php
@@ -57,11 +57,11 @@ $utilisateurs_affiches = array_slice($utilisateurs, $debut, $utilisateurs_par_pa
     <!-- Pagination -->
     <div class="pagination">
         <?php if ($page > 1): ?>
-            <a href="?page=<?= $page - 1 ?>">← Page précédente</a>
+            <a href="?page=<?= $page - 1 ?>" class="pages">Page précédente</a>
         <?php endif; ?>
 
         <?php if ($debut + $utilisateurs_par_page < count($utilisateurs)): ?>
-            <a href="?page=<?= $page + 1 ?>">Page suivante →</a>
+            <a href="?page=<?= $page + 1 ?>" class="pages">Page suivante</a>
         <?php endif; ?>
     </div>
 
